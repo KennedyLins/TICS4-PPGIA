ca238d70df33c31b11eb3d1b44260d2769e7e6a4
Kennedy Lins <kennedy.lins@gmail.com>
2022-10-19 06:46:51 -0300

    ajustando gráfico

--- app.py
+++ app.py
@@ -1,19 +1,46 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
 
 st.set_page_config(
-    page_title = "Teste do Streamlit por Gabriel Alves",
+    page_title = "Dados Meteorológicos da Cidade do Recife",
     layout = "wide",
     menu_items = {
         'About': "TESTE DO ABOUT"
     }
 )
 
-nome = st.text_input('Digite o seu nome:')
-if nome == '':
-    st.markdown('O STREAMLIT RODOU!')
-else:
-    st.markdown(f'''
-        Oi, <b style="color:#0033FF; text-transform:uppercase">{nome}</b>.<br>
-        O STREAMLIT RODOU!<br>
-        E ele aceita <code>HTML</code> e <code>CSS</code>!
-    ''', unsafe_allow_html=True)
+
+st.title('Dados Meteorológicos da Cidade do Recife')
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
